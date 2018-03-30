30ca498de9e109c429a084eb38c91b1f648b6696
Unknown <pet5631@gmail.com>
2018-03-30 12:02:21 +0200

    Added new code for shifts
    Shifts does not work and I dont know why...

--- VHDL/ALU/My_first_ALU.vhd
+++ VHDL/ALU/My_first_ALU.vhd
@@ -16,12 +16,14 @@
 ---- Revision 0.01 - File Created
 ---- Additional Comments:
 ----
-----	Denne fil ligger i GIT-mappe
+---- Denne fil ligger i GIT-mappe
 ------------------------------------------------------------------------------------
 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
+use ieee.numeric_std.all; 
+
 -- This is a simple ALU.
 -- It has:
 -- OPERATIONS:
@@ -32,39 +34,35 @@ USE IEEE.NUMERIC_STD.ALL;
 -- XOR
 -- Negate A
 -- Negate B
--- Logic shift left   * To be implemented
--- Logic shift right  * To be implemented
--- Arith shift left	 * To be implemented
--- Arith shift right  * To be implemented
--- INC A					 * To be implemented
--- DEC A 				 * To be implemented
-
+-- Logic shift left 		* To be implemented
+-- Logic shift right 	* To be implemented
+-- Arith shift left 		* To be implemented
+-- Arith shift right  	* To be implemented
+-- INC A 					* To be implemented
+-- DEC A 					* To be implemented
 
 -- FLAGS
 -- Zero flag
 -- Overflow flag
 -- Signed flag
--- Parity flag    * To be implemented
-
+-- Parity flag 			* To be implemented
 ENTITY My_first_ALU IS
-	GENERIC 
-	(
-		ADD    : std_logic_vector := "0000";
-		SUB    : std_logic_vector := "0001";
-		OG     : std_logic_vector := "0010";
-		ELLER  : std_logic_vector := "0011";
-		XELLER : std_logic_vector := "0100";
-		NOT_A  : std_logic_vector := "0101";
-		NOT_B  : std_logic_vector := "0110";
-		LSL	: std_logic_vector := "0111";
-		LSR	: std_logic_vector := "1000";
-		ASL	: std_logic_vector := "1001";
-		ASR	: std_logic_vector := "1010";
-		INC	: std_logic_vector := "1011";
-		LSL	: std_logic_vector := "1100"
+	GENERIC (
+		ADD    : std_logic_vector := "0000"; -- Adds two operands
+		SUB    : std_logic_vector := "0001"; -- Subtracts two operands
+		OG     : std_logic_vector := "0010"; -- ANDs two operands
+		ELLER  : std_logic_vector := "0011"; -- ORs two operands
+		XELLER : std_logic_vector := "0100"; -- XORs two operands
+		NOT_A  : std_logic_vector := "0101"; -- Negates operand A
+		NOT_B  : std_logic_vector := "0110"; -- Negates operand B
+		LSL    : std_logic_vector := "0111"; -- Logic Shift Left Operand A by Operand B number of bits. Fill with "0"
+		LSR    : std_logic_vector := "1000"; -- Logic Shift Right Operand A by Operand B number of bits. Fill with "0"
+		ASL    : std_logic_vector := "1001"; -- Arithmetic Shift Left Operand A by Operand B number of bits. Fill with right bit
+		ASR    : std_logic_vector := "1010"; -- Arithmetic Shift ri Operand A by Operand B number of bits. Fill with left bit
+		INC_A  : std_logic_vector := "1011"; -- Increments A
+		DEC_A  : std_logic_vector := "1100"  -- Decrements A
 	);
-	PORT 
-	(
+	PORT (
 		Operand1, Operand2 : IN std_logic_vector(15 DOWNTO 0); -- Operands 1 and 2
 		Operation          : IN std_logic_vector(3 DOWNTO 0);
 
@@ -81,7 +79,7 @@ ARCHITECTURE Behavioral OF My_first_ALU IS
 
 	SIGNAL Temp          : std_logic_vector(16 DOWNTO 0); -- Used to store results when adding. Has room for the carry
 	SIGNAL Overflow_temp : std_logic_vector(15 DOWNTO 0); -- Used to store the value of the overflow flag.
- 
+
 BEGIN
 	PROCESS (Operand1, Operand2, Operation, temp) IS
 	BEGIN
@@ -101,19 +99,19 @@ BEGIN
 				-- This is done to make room for the sign-bit/carry bit.
 				Result        <= Temp(15 DOWNTO 0);
 				Carry_Out     <= Temp(16);
-				Overflow_Flag <= ((Operand1(15)) OR (Temp(15))) AND ((NOT (Operand2(15))) OR(NOT (Temp(15)))) AND ((NOT (Operand1(15))) OR ((Operand2(15)))); 
+				Overflow_Flag <= ((Operand1(15)) OR (Temp(15))) AND ((NOT (Operand2(15))) OR(NOT (Temp(15)))) AND ((NOT (Operand1(15))) OR ((Operand2(15))));
 				-- http://www.c-jump.com/CIS77/CPU/Overflow/lecture.html Her står om overflow detection
- 
+
 			WHEN SUB => -- res = |op1 - op2|, flag = 1 iff op2 > op1, Zero = 1 if op2 = op1
 				IF (Operand1 > Operand2) THEN
 					Temp          <= std_logic_vector(signed("0" & Operand1) - signed(Operand2));
-					Result        <= Temp(15 downto 0);
+					Result        <= Temp(15 DOWNTO 0);
 					Signed_Flag   <= '0'; -- Flag raised if result is negative
 					Overflow_Flag <= ((Operand1(15)) OR (Operand2(15))) AND ((NOT (Operand2(15))) OR((Temp(15)))) AND ((NOT (Operand1(15))) OR (NOT (Temp(15))));
- 
+
 				ELSE
 					Temp          <= std_logic_vector(signed("0" & Operand1) - signed(Operand2));
-					Result        <= Temp(15 downto 0);
+					Result        <= Temp(15 DOWNTO 0);
 					Overflow_Flag <= ((Operand1(15)) OR (Operand2(15))) AND ((NOT (Operand2(15))) OR((Temp(15)))) AND ((NOT (Operand1(15))) OR (NOT (Temp(15))));
 					IF (Operand1 < Operand2) THEN
 						Signed_Flag <= '1'; -- Flag raised if result is negative
@@ -122,33 +120,36 @@ BEGIN
 					END IF;
 				END IF;
 
-			WHEN OG => 
+			WHEN OG => -- Returns Operand1 AND Operand2
 				Result <= Operand1 AND Operand2;
 
-			WHEN ELLER => 
+			WHEN ELLER => -- Returns Operand1 OR Operand2
 				Result <= Operand1 OR Operand2;
 
-			WHEN XELLER => 
+			WHEN XELLER => -- Returns Operand1 XOR Operand2
 				Result <= Operand1 XOR Operand2;
 
-			WHEN NOT_A => 
+			WHEN NOT_A => -- Returns NOT Operand1
 				Result <= NOT Operand1;
 
-			WHEN NOT_B => 
+			WHEN NOT_B => -- Returns NOT Operand2
 				Result <= NOT Operand2;
-				
+ 
 			WHEN LSL => -- Logic Shift Left Operand1 by Operand2 number of bits. Fill with "0"
-				Result <= Operand1 sll Operand2;
+				Overflow_temp   <= std_logic_vector(signed(Operand1) sll signed(Operand2));
+				Result <= Overflow_temp(15 DOWNTO 0);
 				
-			WHEN LSR => -- Logic Shift Right Operand1 by Operand2 number of bits. Fill with "0"
-				Result <= Operand1 srl Operand2;
+--			WHEN LSR => -- Logic Shift Right Operand1 by Operand2 number of bits. Fill with "0"
+--				Overflow_temp   <= std_logic_vector(unsigned(Operand1) srl unsigned(Operand2));
+--				Result <= Overflow_temp(15 DOWNTO 0);
 				
 			WHEN LSL => -- Arithmetic Shift Left Operand1 by Operand2 number of bits. Fill with right bit
-				Result <= Operand1 sla Operand2;
+				Overflow_temp   <= std_logic_vector(signed(Operand1) sla signed(Operand2));
+				Result <= Overflow_temp(15 DOWNTO 0);  
 				
 			WHEN LSL => -- Arithmetic Shift ri Operand1 by Operand2 number of bits. Fill with left bit
-				Result <= Operand1 sra Operand2;
-						
+				Overflow_temp   <= std_logic_vector(signed(Operand1) sra signed(Operand2));
+				Result <= Overflow_temp(15 DOWNTO 0);
 
 			WHEN OTHERS => 
 
