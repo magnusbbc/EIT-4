8bc8cbe3f36be03a5203ff3697de656b46b0dfe8
Unknown <magnus@bogh.dk>
2018-04-05 12:13:28 +0200

    improved master and test bench

--- VHDL/Master/Master.vhd
+++ VHDL/Master/Master.vhd
@@ -5,29 +5,30 @@ USE IEEE.NUMERIC_STD.ALL;
 ENTITY Master IS
 	PORT (
 		clk : std_logic;
-		op : IN std_logic_vector(31 DOWNTO 26); -- Operands 1 and 2
-		cnt: OUT std_logic_vector(13 DOWNTO 0);
-		PDO: OUT std_logic_vector(31 downto 0)
 	);
 END ENTITY Master;
 
 ARCHITECTURE Behavioral OF Master IS
-		--SIGNAL PDO : std_logic_vector(15 downto 0);
-		SIGNAL PDI : std_logic_vector(31 downto 0);
+		SIGNAL INSTRUCTION : std_logic_vector(31 downto 0);
+		SIGNAL PDI : std_logic_vector(15 downto 0);
 		SIGNAL PADDR : std_logic_vector(9 downto 0);
 		SIGNAL PEN   : STD_LOGIC := '0';
 		SIGNAL PRW	 : STD_LOGIC := '0';
 		
+		SIGNAL CONTROL : STD_logic_vector(13 downto 0);
 		SIGNAL PC : INTEGER :=0 ;
 		SIGNAL SW : STD_LOGIC := '0';
 BEGIN
-	CONTROLLER : ENTITY work.Control(Behavioral) PORT MAP(opcode => op, cntSignal => cnt);
-	PMEMORY    : ENTITY work.Memory(Behavioral) PORT MAP(DO => PDO, DI => PDI, Address => PADDR, EN => PEN, RW => PRW, clk => clk);
+	CONTROLLER : ENTITY work.Control(Behavioral) PORT MAP(opcode => INSTRUCTION, cntSignal => cnt);
+	PMEMORY    : ENTITY work.Memory(Behavioral) PORT MAP(DO => INSTRUCTION, DI => PDI, Address => PADDR, EN => PEN, RW => PRW);
+	REGFILE	  : ENTITY work.Registers(Behavioral) PORT MAP(R1I => , R2I => , R1O => , R2O => , R3I => , R4I => , DI3 => , DI4 => , WE3 => , WE4 => );
+	ALUCONTROL : ENTITY work.AluCnt(Behavioral) PORT MAP(InsOp => , CntOp => , AluOp => );
+	ALU 		  : ENTITY work.ALU(Behavioral) PORT MAP();
 	
 	onEdge : PROCESS(clk)
 		BEGIN
 			IF (rising_edge(clk)) THEN
-				IF(SW = '0') THEN
+				IF(SW = '0') THEN --Write instructions to program memory
 					PADDR <= std_logic_vector(to_unsigned(PC, PADDR'length));
 					PDI <= std_logic_vector(to_unsigned(PC, PDI'length));
 					PRW  <= '1';
@@ -39,10 +40,11 @@ BEGIN
 					END IF;
 				END IF;
 				
-				IF(SW = '1') THEN
+				IF(SW = '1') THEN --Execute Program
 					PADDR <= std_logic_vector(to_unsigned(PC, PADDR'length));
 					PRW  <= '0';
 					PEN <= '1';
+					
 					PC <= PC+1;
 					IF(PC =4) THEN
 						SW <= '1';
--- VHDL/Test Benches/Control_TB.vhd
+++ VHDL/Test Benches/Control_TB.vhd
@@ -8,11 +8,6 @@ END Control_TB;
 ARCHITECTURE Behavioral OF Control_TB IS
 	 
 	SIGNAL MO : std_logic_vector(15 downto 0);
-	--Input
-	SIGNAL input : std_logic_vector(5 DOWNTO 0) := (others => '0'); 
-	
-	--Output
-	SIGNAL output : std_logic_vector(13 DOWNTO 0); 
 	
 	--Clock Constants
 	constant TbPeriod : time := 10 ns;
@@ -20,7 +15,7 @@ ARCHITECTURE Behavioral OF Control_TB IS
    signal TbSimEnded : std_logic := '0';
 	signal cnt : integer := 0;
 BEGIN
-	MAIN : ENTITY work.Master(Behavioral) PORT MAP(op =>input , cnt => output, clk=>TbClock, PDO => MO);
+	MAIN : ENTITY work.Master(Behavioral) PORT MAP(clk=>TbClock, PDO => MO);
 	
 	-- Clock generation
    TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
@@ -29,9 +24,8 @@ BEGIN
 stim_proc:process(TbClock)
 		begin
 			IF(rising_edge(TbClock)) THEN
-			input<=std_logic_vector(to_unsigned(cnt, input'length));
 			cnt<=cnt+1;
-				IF(cnt = 80) THEN
+				IF(cnt = 8) THEN
 						TbSimEnded <= '1';
 						--wait;
 				END IF; 
