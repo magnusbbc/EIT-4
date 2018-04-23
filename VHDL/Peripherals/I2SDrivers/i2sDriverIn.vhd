4f15d68d112b407bf98e3fcbbd197bcb21a6d8df
Unknown <frizzer@live.dk>
2018-04-23 19:25:23 +0200

    I2S comments
    Added essential comments

--- VHDL/Peripherals/I2SDrivers/i2sDriverIn.vhd
+++ VHDL/Peripherals/I2SDrivers/i2sDriverIn.vhd
@@ -1,98 +1,96 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
-ENTITY i2sDriverIn IS
-	GENERIC
+entity i2sDriverIn is
+	generic
 	(
-		DATA_WIDTH : INTEGER RANGE 4 TO 32 := 16
+		DATA_WIDTH : integer range 4 to 32 := 16
 	);
-	PORT
+	port
 	(
-		bclk   : IN std_logic;
-		ws     : IN std_logic;
-		Din    : IN std_logic;
-		DOut_L : OUT std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
-		DOut_R : OUT std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
-		int_L  	: out std_logic :='0';
-		int_R  	: out	std_logic :='0';
-		intr_L  	: in std_logic :='0';
-		intr_R  	: in std_logic :='0'
+		bclk   : in std_logic;-- Bitclock in
+		ws     : in std_logic;-- Worselect
+		Din    : in std_logic;-- Data in
+		DOut_L : out std_logic_vector(DATA_WIDTH - 1 downto 0); -- One full word of data out
+		DOut_R : out std_logic_vector(DATA_WIDTH - 1 downto 0);
+		int_L  : out std_logic := '0';-- Interupt out. Is set high when a new word is ready
+		int_R  : out std_logic := '0';
+		intr_L : in std_logic  := '0';-- Interupt reset. Set this high to reset the interupt. Should be high untill intterupt put is low again.
+		intr_R : in std_logic  := '0'
 		--Ports for input
 	);
-END i2sDriverIn;
+end i2sDriverIn;
 
-ARCHITECTURE i2sDriverIn OF i2sDriverIn IS
-	SIGNAL lr  : std_logic := '1';
-	SIGNAL cnt : INTEGER   := 0;
-	signal outBuff : std_logic_vector ( DATA_WIDTH -1 downto 0);
-	
-BEGIN
-	data_in : PROCESS (bclk, ws)
-	variable vcnt : integer :=0;
-	variable voutBuff : std_logic_vector ( DATA_WIDTH -1 downto 0);
-	BEGIN
-		IF rising_edge(bclk) THEN
-			vcnt:= cnt;
-			voutBuff:=outBuff;
-			
+architecture i2sDriverIn of i2sDriverIn is
+	signal lr      : std_logic := '1';
+	signal cnt     : integer   := 0;
+	signal outBuff : std_logic_vector (DATA_WIDTH - 1 downto 0);
+
+begin
+	data_in : process (bclk, ws)
+		variable vcnt     : integer := 0;
+		variable voutBuff : std_logic_vector (DATA_WIDTH - 1 downto 0);
+	begin
+		if rising_edge(bclk) then
+			vcnt     := cnt;
+			voutBuff := outBuff;
+
 			if intr_L = '1' then
-			int_L <='0';
+				int_L <= '0';
 			end if;
-			if intr_R ='1' then
-			int_R<='0';
+			if intr_R = '1' then
+				int_R <= '0';
 			end if;
-			
-			
-			IF vcnt < DATA_WIDTH THEN
+			if vcnt < DATA_WIDTH then
 
 				voutBuff(DATA_WIDTH - vcnt - 1) := Din;
 
-				vcnt := vcnt + 1;
+				vcnt                            := vcnt + 1;
 
-			END IF;
+			end if;
 
-			IF lr = NOT ws THEN
-				
+			if lr = not ws then
+
 				--IF vcnt < DATA_WIDTH THEN
-					IF lr = '1' THEN
-						dout_L<=voutbuff;
-						--dout_L(DATA_WIDTH - vcnt - 1 DOWNTO 0) <= (OTHERS => '0');
-					ELSE
-						dout_R<=voutbuff;
-						--dout_R(DATA_WIDTH - vcnt - 1 DOWNTO 0) <= (OTHERS => '0');
-					END IF;
-					
+				if lr = '1' then
+					dout_L <= voutbuff;
+					--dout_L(DATA_WIDTH - vcnt - 1 DOWNTO 0) <= (OTHERS => '0');
+				else
+					dout_R <= voutbuff;
+					--dout_R(DATA_WIDTH - vcnt - 1 DOWNTO 0) <= (OTHERS => '0');
+				end if;
+
 				--END IF;
 
-				lr  <= ws;
+				lr <= ws;
 
 				vcnt := 0;
 
-				IF ws = '0' THEN
+				if ws = '0' then
 
 					int_R <= '1';
 					int_L <= '0';
 
-				ELSE
+				else
 
 					int_R <= '0';
 					int_L <= '1';
-				END IF;
-			ELSE
-				IF ws = '1' THEN
+				end if;
+			else
+				if ws = '1' then
 
 					int_R <= '1';
 					int_L <= '0';
 
-				ELSE
+				else
 
 					int_R <= '0';
 					int_L <= '1';
-				END IF;
-				
-			END IF;
-			cnt<=vcnt;
-			outBuff<=voutBuff;
-		END IF;
-	END PROCESS;
-END i2sDriverIn;
+				end if;
+
+			end if;
+			cnt     <= vcnt;
+			outBuff <= voutBuff;
+		end if;
+	end process;
+end i2sDriverIn;
--- VHDL/Peripherals/I2SDrivers/i2sDriverOut.vhd
+++ VHDL/Peripherals/I2SDrivers/i2sDriverOut.vhd
@@ -1,103 +1,89 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
-ENTITY i2sDriverOut IS
-	GENERIC
+entity i2sDriverOut is
+	generic
 	(
-		DATA_WIDTH : INTEGER RANGE 4 TO 32 := 16
+		DATA_WIDTH : integer range 4 to 32 := 16
 	);
-	PORT
+	port
 	(
-		
-		clk		: in std_logic;
-		int_L  	: in 	std_logic :='0';
-		int_R  	: in 	std_logic :='0';
-		intr_L  	: out std_logic :='0';
-		intr_R  	: out std_logic :='0';
-		DIn_L 	: in std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
-		DIn_R 	: in std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
-		bclk     : out std_logic;
-		ws       : out std_logic;
-		DOut     : out std_logic
-		
+
+		clk    : in std_logic;-- Clock that will be used for logic and will be directly used as output bitclock. Can be directly connected to a syncronized bitclock input from the i2s input signal.
+		int_L  : in std_logic  := '0';-- Interupt for when  new signals are stable at input. should be set high when a new word should be loaded into the output buffer.
+		int_R  : in std_logic  := '0';
+		intr_L : out std_logic := '0';--Interupt reset. Input is set to high when the input has been loaded to the input buffer.
+		intr_R : out std_logic := '0';
+		DIn_L  : in std_logic_vector(DATA_WIDTH - 1 downto 0);-- The data input buses. The word that should be loaded into the buffer should be loaded to these inputs when the interupts are set to high.
+		DIn_R  : in std_logic_vector(DATA_WIDTH - 1 downto 0);
+		bclk   : out std_logic;-- Output bitclock for the output i2s signal.
+		ws     : out std_logic;--Output wordselect for the output i2s signal.
+		DOut   : out std_logic-- Output serial data for the i2s signal.
+
 	);
-END i2sDriverOut;
+end i2sDriverOut;
 
-ARCHITECTURE i2sDriverOut OF i2sDriverOut IS
-	signal clkb	: std_logic :='1';
-	signal buff_In_L 	: std_logic_vector(DATA_WIDTH - 1 DOWNTO 0):= (1=> '1',others => '1');
-	signal buff_In_R 	: std_logic_vector(DATA_WIDTH - 1 DOWNTO 0):= (1=> '1',others => '1');
-	SIGNAL cnt : INTEGER   := 0;
-	Signal lr : std_logic := '0';
-BEGIN
+architecture i2sDriverOut of i2sDriverOut is
+	signal clkb      : std_logic                                 := '1';
+	signal buff_In_L : std_logic_vector(DATA_WIDTH - 1 downto 0) := (1 => '1', others => '1');
+	signal buff_In_R : std_logic_vector(DATA_WIDTH - 1 downto 0) := (1 => '1', others => '1');
+	signal cnt       : integer                                   := 0;
+	signal lr        : std_logic                                 := '0';
+begin
 
-bclk<=clk;
+	bclk <= clk;
 
-data_out : PROCESS (clk)
-	BEGIN
-		IF falling_edge(clk) THEN
-		
-		--clkb <= not clkb;							-- Clock logic
-		--	bclk<= clkb;
-			
-			--if clkb ='0' then							--The Master does most of the work on the falling edge
+	data_out : process (clk)
+	begin
+		if falling_edge(clk) then
+
 			if cnt < DATA_WIDTH then
-			if lr ='1' then					--Write from the active buffer 
-				DOut <= buff_In_L(DATA_WIDTH - 1-cnt);
-			else
-				DOut <= buff_In_R(DATA_WIDTH - 1-cnt);
+				if lr = '1' then --Write from the active buffer 
+					DOut <= buff_In_L(DATA_WIDTH - 1 - cnt);
+				else
+					DOut <= buff_In_R(DATA_WIDTH - 1 - cnt);
+				end if;
+
 			end if;
-			
-			end if;
-			
-			if cnt +1 >= DATA_WIDTH then	--Change channel
+
+			if cnt + 1 >= DATA_WIDTH then --Change channel
 				ws <= not lr;
 				lr <= not lr;
-						
-			end if;
-			
-		
-		END IF;
-		
-	END PROCESS;
-	
-	data_out_cnt : PROCESS (clk)
-	BEGIN
-		IF rising_edge(clk) THEN
-		
-		--clkb <= not clkb;							-- Clock logic
-		--	bclk<= clkb;
-			
-			
-			
-			cnt<=cnt+1;
-			
-			
-			
-			if cnt +1 >= DATA_WIDTH then	--Change channel
 
-				cnt <= 0;				
 			end if;
+		end if;
+
+	end process;
+
+	data_out_cnt : process (clk)
+	begin
+		if rising_edge(clk) then
+
 			
-			
-			if (int_L='1') and(cnt=datA_WIDTH-1) then 						--Update the word that is ready
-				buff_In_L<=DIn_L;
-				intr_L <='1';
-			elsif (int_R = '1') and(cnt=datA_WIDTH-1) then
-				buff_In_R<=DIn_R;
-				intr_R <='1';
+			cnt <= cnt + 1;
+
+			if cnt + 1 >= DATA_WIDTH then --Change channel
+
+				cnt <= 0;
 			end if;
-			
-			if (int_L='0') then 						--Update the word that is ready
-				intr_L <='0';
+			if (int_L = '1') and(cnt = datA_WIDTH - 1) then --Update the word that is ready
+				buff_In_L <= DIn_L;
+				intr_L    <= '1';
+			elsif (int_R = '1') and(cnt = datA_WIDTH - 1) then
+				buff_In_R <= DIn_R;
+				intr_R    <= '1';
+			end if;
+
+			if (int_L = '0') then --Update the word that is ready
+				intr_L <= '0';
 			end if;
 			if (int_R = '0') then
-				intr_R <='0';
+				intr_R <= '0';
 			end if;
 			--end if;
-		
-		END IF;
-		
-	END PROCESS;
 
-END i2sDriverOut;
+		end if;
+
+	end process;
+
+end i2sDriverOut;
